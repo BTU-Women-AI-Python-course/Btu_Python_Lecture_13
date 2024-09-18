d4e51903c21c8a0ae92ed44297f24d18f10b6816
NikaMdinaradze <mdinaradzenika04@gmail.com>
2024-09-18 21:09:46 +0400

    add products create view v3

--- nikoloz_mdinaradze_lecture/products/forms.py
+++ nikoloz_mdinaradze_lecture/products/forms.py
@@ -1,8 +1,20 @@
 from django import forms
+from django.core.exceptions import ValidationError
+
+from products.models import Product
 
 
-class ProductForm(forms.Form):
-    name = forms.CharField(max_length=255)
-    description = forms.CharField(max_length=255)
-    price = forms.FloatField()
-    category = forms.CharField(max_length=255)
+def validate_name(value):
+    if value.isnumeric():
+        raise ValidationError(
+            "%(value)s is numeric.",
+            params={"value": value},
+        )
+
+
+class ProductForm(forms.ModelForm):
+    name = forms.CharField(max_length=255, validators=[validate_name])
+
+    class Meta:
+        model = Product
+        fields = ["name", "description", "price", "category"]
--- nikoloz_mdinaradze_lecture/products/views.py
+++ nikoloz_mdinaradze_lecture/products/views.py
@@ -1,7 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from products.models import Product
 from products.forms import ProductForm
 
 
@@ -10,14 +9,8 @@ def create_product(request):
     if request.method == "POST":
         form = ProductForm(request.POST)
         if form.is_valid():
-            data = form.cleaned_data
-            Product.objects.create(
-                name=data['name'],
-                description=data['description'],
-                price=data['price'],
-                category=data['category']
-            )
-        return HttpResponse("Product created !!!")
+            form.save()
+            return HttpResponse("Product created !!!")
     else:
         form = ProductForm()
     return render(request, "products/products_create.html", {"form": form})
