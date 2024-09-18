a8e4e60db6d6576380750bf26a8ec0f1449bfe0a
NikaMdinaradze <mdinaradzenika04@gmail.com>
2024-09-18 20:05:52 +0400

    add products create view v1

--- nikoloz_mdinaradze_lecture/products/views.py
+++ nikoloz_mdinaradze_lecture/products/views.py
@@ -1,3 +1,18 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+from products.models import Product
+
+
 # Create your views here.
+def create_product(request):
+    if request.method == "POST":
+        Product.objects.create(
+            name=request.POST['name'],
+            description=request.POST['description'],
+            price=request.POST['price'],
+            category=request.POST['category']
+        )
+        return HttpResponse("Product created !!!")
+    return render(request, "products/products_create.html")
+
